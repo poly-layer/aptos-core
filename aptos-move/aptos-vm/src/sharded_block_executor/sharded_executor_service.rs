bf269e76354a2e1d96e8ccfc9ee6ae383a2393aa
janolk <jan.olkowski.a@gmail.com>
2024-06-12 18:33:37 -0700

    Display executor TPS metrics after each 50th block

--- aptos-move/aptos-vm/src/sharded_block_executor/sharded_executor_service.rs
+++ aptos-move/aptos-vm/src/sharded_block_executor/sharded_executor_service.rs
@@ -226,7 +226,7 @@ impl<S: StateView + Sync + Send + 'static> ShardedExecutorService<S> {
             self.shard_id,
             self.num_shards
         );
-
+        let mut cnt = 0;
         let mut cumulative_txns = 0;
         loop {
            // info!("Looping back to recv cmd after execution of a block********************");
@@ -348,6 +348,20 @@ impl<S: StateView + Sync + Send + 'static> ShardedExecutorService<S> {
                 txn_output: TransactionOutput::default(),
             }).unwrap();
             stream_results_thread.join().unwrap();
+            if (cnt % 50 == 49) {
+                let exe_time = SHARDED_EXECUTOR_SERVICE_SECONDS
+                    .get_metric_with_label_values(&[&self.shard_id.to_string(), "execute_block"])
+                    .unwrap()
+                    .get_sample_sum();
+                 info!(
+                    "Shard {} is shutting down; On shard execution tps {} txns/s ({} txns / {} s)",
+                    self.shard_id,
+                    (cumulative_txns as f64 / exe_time),
+                    cumulative_txns,
+                    exe_time
+                );
+            }
+            cnt = cnt + 1;
         }
 
         let exe_time = SHARDED_EXECUTOR_SERVICE_SECONDS
