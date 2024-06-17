58230100b2cbfa07f6ab740ad74e9e6aaaec309d
janolk <jan.olkowski.a@gmail.com>
2024-06-17 09:04:49 -0700

    check qcp queue avg len, sync_insert_keys=true, cmd_tx_threads=16, batch_size=200, rx_threads=8

--- execution/executor-service/src/remote_cordinator_client.rs
+++ execution/executor-service/src/remote_cordinator_client.rs
@@ -171,7 +171,8 @@ impl RemoteCoordinatorClient {
                         let batch_start_index = txns.batch_start_index;
                         let state_keys = Self::extract_state_keys_from_txns(&transactions);
 
-                        state_view_client_clone.pre_fetch_state_values(state_keys, false);
+                        state_view_client_clone.pre_fetch_state_values(state_keys, true);
+                        // state_view_client_clone.pre_fetch_state_values(state_keys, false);
 
                         let _ = transactions.into_iter().enumerate().for_each(|(idx, txn)| {
                             blocking_transactions_provider_clone.set_txn(idx + batch_start_index, txn);
